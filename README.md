88204b45ec2f2e1e0e59b1fd9154b94b7e5355f0
Robert Chen <bergintonatcapital@gmail.com>
2023-03-28 21:46:28 -0400

    Create functionality to add form's book info to table

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 A virtual library app to store info on the books you have read or will read. This project is aim to demonstrate my new found JS knowledge ~ working with objects, prototypes, and classes. Now, put your favorite books from the vault of knowledge into this program build with knowledge!
 
+## Other Goals:
+Utilized tools such as esLint and prettier. Exploring Airbnb JavaScript Style Guide.
+
--- script.js
+++ script.js
@@ -1,36 +1,41 @@
-let myLibrary = [];
-
 class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    } 
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+const myLibrary = [];
+
+// Display the book from array to table
+function displayLibrary(bookObject) {
+  const table = document.getElementById('books-table');
+  const tableRow = table.insertRow();
+
+  Object.keys(bookObject).forEach((info) => {
+    const tableCell = tableRow.insertCell();
+    const bookInfo = document.createTextNode(bookObject[info]);
+    tableCell.appendChild(bookInfo);
+  });
 }
 
-let submit = document.querySelector('button[type="submit"]');
-submit.addEventListener('click', createBookInfo);
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  myLibrary.forEach(displayLibrary);
+}
 
 function createBookInfo(event) {
-    event.preventDefault();     // stop the form from submitting & refreshing page
+  event.preventDefault(); // stop the form from submitting & refreshing page
 
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const pages = document.getElementById('pages').value;
-    const read = document.getElementById('read').value;
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.getElementById('read').value;
 
-    const book = new Book(title, author, pages, read);
-    addBookToLibrary(book);
+  const book = new Book(title, author, pages, read);
+  addBookToLibrary(book);
 }
 
-// create a new book object & add it to the array
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-    console.log(myLibrary)
-}
-
-// Display the book from array on screen
-function displayLibrary() {
-
-}
+const submit = document.querySelector('button[type="submit"]');
+submit.addEventListener('click', createBookInfo);
